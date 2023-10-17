c56eb0eb734c943373c02907a0f518fa88acfb7f
Smooth228105 <Smooth228105@yandex.ru>
2023-10-17 11:41:05 +0300

    task10

---
+++ src/task10.js
@@ -0,0 +1,7 @@
+const brush1 = 500;
+const brush2 = 3500
+const friend = 5;
+const tips = 500;
+const payment2 = brush2 + tips;
+const payment1 = payment2 / friend
+console.log("с каждого", payment1);
